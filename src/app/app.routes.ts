75fc3a911b86e4bace47b2f5b71230743f0aa88b
nekaaa <nekaaabelkader1987@gmail.com>
2025-09-16 14:57:35 +0100

    add nav bar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { JobsComponent } from './pages/jobs/jobs.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { CandidateDashboardComponent } from './pages/candidate-dashboard/candidate-dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+
+{ path: '', component: HomeComponent },
+  { path: 'jobs', component: JobsComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'dashboard', component: CandidateDashboardComponent },
+
+
+
+
+
+
+
+
+
+
+
+
+];
---
+++ src/app/pages/candidate-dashboard/candidate-dashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-candidate-dashboard',
+  imports: [],
+  templateUrl: './candidate-dashboard.component.html',
+  styleUrl: './candidate-dashboard.component.css'
+})
+export class CandidateDashboardComponent {
+
+}
